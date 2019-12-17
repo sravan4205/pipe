d795c3fedcfd1022948e13231ac812f1842adbec
sravan4205 <sravan.amishetti@gmail.com>
2019-12-17 18:25:11 +0530

    pipe push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyOrderByPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipe';
+
+  constructor(private orderPipe: MyOrderByPipe) {
+    console.log("order by digit");
+    console.log(this.orderPipe.transform(this.items, 'digit'));
+  } 
+
+  items = [
+    { id: "1", name: "sravan", age: 24 },
+    { id: "2", name: "sumangth", age: 25 },
+    { id: "3", name: "naveen", age: 26 },
+  ];
+
+  order = "id";
+  reverse = false;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyOrderByPipe } from './sort.pipe';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, MyOrderByPipe],
+  bootstrap: [AppComponent],
+  entryComponents: [],
+  providers: [MyOrderByPipe]
 })
 export class AppModule { }
